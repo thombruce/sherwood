74f05bd404f1b2dce27c2eee892ccee1a22f2c95
Thom Bruce <thom@thombruce.com>
2026-01-20 14:13:22 +0000

    refactor: Move core Sherwood commands module into core/

--- crates/sherwood/src/core/mod.rs
+++ crates/sherwood/src/core/mod.rs
@@ -1,3 +1,4 @@
 pub mod content_generation;
 pub mod markdown_config;
+pub mod sherwood;
 pub mod utils;
--- crates/sherwood/src/lib.rs
+++ crates/sherwood/src/lib.rs
@@ -5,11 +5,10 @@ pub mod generator;
 pub mod plugins;
 pub mod presentation;
 pub mod server;
-mod sherwood;
 pub mod templates;
 
 pub use config::{SiteConfig, SiteSection, TemplateSection};
-pub use core::content_generation;
+pub use core::{content_generation, sherwood};
 pub use generator::{
     generate_site, generate_site_development, generate_site_development_with_plugins,
     generate_site_development_with_plugins_and_templates, generate_site_with_plugins,
@@ -20,6 +19,7 @@ pub use presentation::styles::StyleManager;
 pub use server::{
     run_dev_server, run_dev_server_with_plugins, run_dev_server_with_plugins_and_templates,
 };
-pub use sherwood::Sherwood;
 pub use templates::TemplateManager;
 pub use templates::{TemplateRegistry, partials, partials::ContentItem};
+
+pub use sherwood::Sherwood;
